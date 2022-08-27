c7f8ac09edced2be343947c918a6f09f49943164
Tebiremen <okojie.aitebiremen@gmail.com>
2022-08-27 09:54:25 -0700

    We shall live on the intranet

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
     import hidden_4
-
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    n = dir(hidden_4)
+    for a in n:
+        if a[0:2] != "__":
+            print(a)
