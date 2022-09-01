988bf369e7fba273e0999d75207244765f396392
Tebiremen <okojie.aitebiremen@gmail.com>
2022-09-01 03:30:01 -0700

    Yet to push my last commit

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def sqaure_matrix_map(matrix=[]):
-    return (list(map(lambda r: list(map(lambda x: x**2, r)), matrix)))
+def square_matrix_map(matrix=[]):
+    return (list(map(lambda x: list(map(lambda y: y**2, x)), matrix)))
